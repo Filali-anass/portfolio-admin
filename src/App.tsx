60ce7b45122edc00e7ece666f19df42935fdb1e3
Filali-anass <anass.filali.93@gmail.com>
2022-07-27 22:31:00 +0100

    Setup Project with ESLINT and typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="">
+      <header className="App-header"></header>
+      <p>OK</p>
+    </div>
+  );
+}
+
+export default App;
